2ea0139fb31edcffa443a3ef53c9678ad077fe08
sinev-valentine <sinev-valentine@yandex.ru>
2022-09-09 19:22:42 +0300

    fix database connect

--- evm_loader/cli/src/rpc/db.rs
+++ evm_loader/cli/src/rpc/db.rs
@@ -2,12 +2,11 @@ use solana_sdk::{
     account::Account,
     pubkey::Pubkey,
 };
-use tokio_postgres::{Client as DBClient, connect, Error};
+use tokio_postgres::{ connect, Error};
 use postgres::{ NoTls };
-// use tokio::task::block_in_place;
 use serde::{Serialize, Deserialize };
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
 pub struct DBConfig{
     pub host: String,
     pub port: String,
@@ -16,59 +15,42 @@ pub struct DBConfig{
     pub password: String,
 }
 
+
 pub struct PostgresClient {
-    client: DBClient,
+    // client: DBClient,
+    config: DBConfig,
     pub slot: u64,
 }
 
 
 impl PostgresClient {
-    // #[allow(unused)]
-    #[tokio::main]
-    pub async fn new(config: &DBConfig, slot: u64) -> Self {
-        let connection_str= format!("host={} port={} dbname={} user={} password={}",
-                                    config.host, config.port, config.database, config.user, config.password);
+    // TODO database connect must be only once
+    pub  fn new(config: &DBConfig, slot: u64) -> Self {
+        Self { config: config.clone() , slot}
+    }
 
-        let (client, connection) =
-            connect(&connection_str, NoTls).await.unwrap();
+    // TODO: remove connection init
+    #[tokio::main]
+    pub async fn get_accounts_at_slot(&self, keys: impl Iterator<Item = Pubkey>) -> Result<Vec<(Pubkey, Account)>, Error> {
+
+        let connection_str= format!("host={} port={} dbname={} user={} password={}",
+                                    self.config.host, self.config.port, self.config.database, self.config.user, self.config.password);
+        let (client, connection) = connect(&connection_str, NoTls).await.unwrap();
 
         tokio::spawn(async move {
             if let Err(e) = connection.await {
                 eprintln!("connection error: {}", e);
             }
         });
-        Self {client, slot}
-    }
 
-    // fn block<F, Fu, R>(&self, f: F) -> R
-    //     where
-    //         F: FnOnce(DBClient) -> Fu,
-    //         Fu: std::future::Future<Output = R>,
-    // {
-    //     let client = self.client.clone();
-    //     block_in_place(|| {
-    //         let handle = tokio::runtime::Handle::current();
-    //         handle.block_on(f(client))
-    //     })
-    // }
-
-    #[tokio::main]
-    pub async fn get_accounts_at_slot(&self, keys: impl Iterator<Item = Pubkey>) -> Result<Vec<(Pubkey, Account)>, Error> {
         let key_bytes = keys.map(|entry| entry.to_bytes()).collect::<Vec<_>>();
         let key_slices = key_bytes.iter().map(|entry| entry.as_slice()).collect::<Vec<_>>();
 
-        let mut result = vec![];
-
-        // let rows = self.block(|| async {
-        //     self.client.query(
-        //         "SELECT * FROM get_accounts_at_slot($1, $2)",&[&key_slices, &(self.slot as i64)]
-        //     ).await
-        // })?;
-
-        let rows = self.client.query(
+        let rows = client.query(
             "SELECT * FROM get_accounts_at_slot($1, $2)",&[&key_slices, &(self.slot as i64)]
         ).await?;
 
+        let mut result = vec![];
         for row in rows {
             let lamports: i64 = row.try_get(2)?;
             let rent_epoch: i64 = row.try_get(4)?;

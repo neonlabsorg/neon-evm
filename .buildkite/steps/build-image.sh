fa35d01aac29591ce77e76f1950f3113031eadbd
sinev-valentine <sinev-valentine@yandex.ru>
2022-10-05 16:29:16 +0300

    empty commit

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 echo "Neon EVM revision=${BUILDKITE_COMMIT}"
 
 #set ${SOLANA_PROVIDER:=solanalabs}
+
 set ${SOLANA_REVISION:=v1.11.10}
 
 #export SOLANA_IMAGE=${SOLANA_PROVIDER}/solana:${SOLANA_REVISION}

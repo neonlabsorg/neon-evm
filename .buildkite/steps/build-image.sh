b7148c64bc256275dd6641ed0bbd3abe741c7740
sinev-valentine <sinev-valentine@yandex.ru>
2022-10-05 16:08:03 +0300

    set custom solana version

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -3,10 +3,12 @@ set -euo pipefail
 
 echo "Neon EVM revision=${BUILDKITE_COMMIT}"
 
-set ${SOLANA_PROVIDER:=solanalabs}
-set ${SOLANA_REVISION:=v1.11.10}
+#set ${SOLANA_PROVIDER:=solanalabs}
+#set ${SOLANA_REVISION:=v1.11.10}
+set ${SOLANA_REVISION:=v1.15.0}
 
-export SOLANA_IMAGE=${SOLANA_PROVIDER}/solana:${SOLANA_REVISION}
+#export SOLANA_IMAGE=${SOLANA_PROVIDER}/solana:${SOLANA_REVISION}
+export SOLANA_IMAGE=neonlabsorg/neon-validator:53511e355c1e8b54c1040a651a879366209550b1
 echo "SOLANA_IMAGE=${SOLANA_IMAGE}"
 docker pull ${SOLANA_IMAGE}
 

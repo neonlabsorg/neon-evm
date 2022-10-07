f03d24a32f5051e99d89d42cc3a1280e6c982a73
sinev-valentine <sinev-valentine@yandex.ru>
2022-10-07 15:48:11 +0300

    set solana to master

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 #export BUILDKITE_COMMIT=9ecbc75f94801009e300f266cf97f3ac475190d6
 echo "Neon EVM revision=${BUILDKITE_COMMIT}"
 
-set ${SOLANA_PROVIDER:=solanalabs}
+#set ${SOLANA_PROVIDER:=solanalabs}
 #set ${SOLANA_REVISION:=v1.11.10}
 set ${SOLANA_REVISION:=v1.14.5}
 
 
-export SOLANA_IMAGE=${SOLANA_PROVIDER}/solana:${SOLANA_REVISION}
-#export SOLANA_IMAGE=neonlabsorg/neon-validator:e2af797e270f4bd619ae9c9d2963628f30f95741
+#export SOLANA_IMAGE=${SOLANA_PROVIDER}/solana:${SOLANA_REVISION}
+export SOLANA_IMAGE=neonlabsorg/neon-validator:4eeef4d31564a8a3914b84a31e40f72f4ff81e31
 #export SOLANA_IMAGE=neonlabs/solana:v1.15.0
 echo "SOLANA_IMAGE=${SOLANA_IMAGE}"
 docker pull ${SOLANA_IMAGE}

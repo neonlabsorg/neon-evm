8146c47d73bb973872454f67c5e1bb8d669e1a05
valiksinev <valiksinev@yahoo.com>
2023-05-05 16:37:14 +0300

    add execution time to log

--- evm_loader/cli/src/main.rs
+++ evm_loader/cli/src/main.rs
@@ -16,6 +16,7 @@ use clap::ArgMatches;
 pub use config::Config;
 
 use crate::errors::NeonCliError;
+use std::time::Instant;
 
 type NeonCliResult = Result<serde_json::Value, NeonCliError>;
 
@@ -50,6 +51,8 @@ fn print_result(result: &NeonCliResult) {
 
 #[tokio::main]
 async fn main() {
+    let time_start = Instant::now();
+
     let options = program_options::parse();
 
     logs::init(&options).expect("logs init error");
@@ -60,6 +63,8 @@ async fn main() {
 
     let result = run(&options);
 
+    let execution_time = Instant::now().duration_since(time_start);
+    log::info!("execution time: {} sec", execution_time.as_secs_f64());
     print_result(&result);
     if let Err(e) = result {
         std::process::exit(e.error_code());

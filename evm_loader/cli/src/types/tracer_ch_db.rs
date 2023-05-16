aa6a335fca47bb1db3ca28dc534fbe5b080e63af
valiksinev <101261445+valiksinev@users.noreply.github.com>
2023-05-16 12:06:58 +0300

    Add delay 100 (#85)
    * impl
    
    * update

--- evm_loader/cli/src/types/tracer_ch_db.rs
+++ evm_loader/cli/src/types/tracer_ch_db.rs
@@ -18,6 +18,8 @@ use std::{
 };
 use thiserror::Error;
 
+const ROOT_BLOCK_DELAY: u8 = 100;
+
 #[derive(Error, Debug)]
 pub enum ChError {
     #[error("clickhouse: {}", .0)]
@@ -36,6 +38,7 @@ pub struct ClickHouseDb {
 pub struct SlotParent {
     pub slot: u64,
     pub parent: Option<u64>,
+    pub status: String,
 }
 
 #[derive(Row, serde::Deserialize, Clone)]
@@ -110,13 +113,26 @@ impl ClickHouseDb {
 
     fn get_branch_slots(&self, slot: u64) -> ChResult<(u64, Vec<u64>)> {
         let query = r#"
-            SELECT distinct on (slot) slot, parent FROM events.update_slot
-            WHERE slot >= (SELECT slot FROM events.update_slot WHERE status = 'Rooted' ORDER BY slot DESC LIMIT 1)
+            SELECT distinct on (slot) slot, parent, status FROM events.update_slot
+            WHERE slot >=
+                (
+                    SELECT slot from events.update_slot WHERE status = 'Rooted' and slot <=
+                    (
+	                    SELECT slot - {} FROM events.update_slot WHERE status = 'Rooted' ORDER BY slot DESC LIMIT 1
+                    )
+                    ORDER BY slot DESC LIMIT 1
+                )
                 and isNotNull(parent)
             ORDER BY slot DESC, status DESC
             "#;
         let time_start = Instant::now();
-        let rows = block(|| async { self.client.query(query).fetch_all::<SlotParent>().await })?;
+        let rows = block(|| async {
+            self.client
+                .query(query)
+                .bind(ROOT_BLOCK_DELAY)
+                .fetch_all::<SlotParent>()
+                .await
+        })?;
         let execution_time = Instant::now().duration_since(time_start);
         info!(
             "get_branch_slot sql(1) time: {} sec",
@@ -129,33 +145,29 @@ impl ClickHouseDb {
         })?;
 
         match slot.cmp(&root.slot) {
-            Less => {
-                let time_start = Instant::now();
-                let count = block(|| async {
-                    let query = "SELECT count(*) FROM events.update_slot WHERE slot = ? and status = 'Rooted'";
-                    self.client.query(query).bind(slot).fetch_one::<u64>().await
-                })?;
-                let execution_time = Instant::now().duration_since(time_start);
-                info!(
-                    "get_branch_slot sql(2) time: {} sec",
-                    execution_time.as_secs_f64()
-                );
-
-                if count == 0 {
-                    let err = clickhouse::error::Error::Custom(format!(
-                        "requested slot is not on working branch {}",
-                        slot
-                    ));
-                    Err(ChError::Db(err))
-                } else {
-                    Ok((slot, vec![]))
-                }
-            }
-            Equal => Ok((root.slot, vec![])),
+            Less | Equal => Ok((slot, vec![])),
             Greater => {
                 let mut branch: Vec<SlotParent> = vec![];
 
+                let mut parent_finalized: Option<u64> = None;
+
                 for row in rows {
+                    if parent_finalized.is_some() {
+                        if row.slot == parent_finalized.unwrap() {
+                            parent_finalized = row.parent;
+                        } else {
+                            if row.status == "Rooted" {
+                                let err = clickhouse::error::Error::Custom(
+                                    "There are several branchs with rooted slots".to_string(),
+                                );
+                                return Err(ChError::Db(err));
+                            }
+                            continue;
+                        }
+                    } else if row.status == "Rooted" {
+                        parent_finalized = row.parent;
+                    }
+
                     if branch.is_empty() {
                         if row.slot == slot {
                             branch.push(row.clone());

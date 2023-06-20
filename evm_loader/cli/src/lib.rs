0e88d225c9f67f97ad7c61bc06e699ac0c3c91af
Yuri Albuquerque <yuridenommus@gmail.com>
2023-06-20 14:06:52 -0300

    Add more modules to neon_lib

--- evm_loader/cli/src/lib.rs
+++ evm_loader/cli/src/lib.rs
@@ -1,1 +1,17 @@
+mod account_storage;
+pub mod commands;
+pub mod config;
+pub mod context;
+mod errors;
+mod event_listener;
+pub mod logs;
+pub mod program_options;
+mod rpc;
+mod syscall_stubs;
 pub mod types;
+
+pub use config::Config;
+pub use context::Context;
+pub use errors::NeonCliError;
+
+pub type NeonCliResult = Result<serde_json::Value, NeonCliError>;

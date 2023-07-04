5f9d4d9d7e9a7ef2244720c771ae67f37e0339d1
valiksinev <valiksinev@yahoo.com>
2023-07-04 18:06:24 +0300

    add debug to GetEtherAccountDataReturn

--- evm_loader/lib/src/commands/get_ether_account_data.rs
+++ evm_loader/lib/src/commands/get_ether_account_data.rs
@@ -7,7 +7,7 @@ use crate::{
     Config, Context, NeonResult,
 };
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct GetEtherAccountDataReturn {
     pub solana_address: String,
     pub address: Address,

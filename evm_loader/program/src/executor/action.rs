3a750752262d1b53106aab5972b4636d2927c573
Stanislav Tkach <stanislav.tkach@gmail.com>
2023-07-17 21:41:21 +0200

    Fix JSON deserialization (serde_json) for the Action type
    feee

--- evm_loader/program/src/executor/action.rs
+++ evm_loader/program/src/executor/action.rs
@@ -17,19 +17,18 @@ pub enum Action {
     NeonTransfer {
         source: Address,
         target: Address,
-        #[serde(with = "ethnum::serde::bytes::le")]
+        #[serde(with = "serde_u256")]
         value: U256,
     },
     NeonWithdraw {
         source: Address,
-        #[serde(with = "ethnum::serde::bytes::le")]
+        #[serde(with = "serde_u256")]
         value: U256,
     },
     EvmSetStorage {
         address: Address,
-        #[serde(with = "ethnum::serde::bytes::le")]
+        #[serde(with = "serde_u256")]
         index: U256,
-        #[serde(with = "serde_bytes_32")]
         value: [u8; 32],
     },
     EvmIncrementNonce {
@@ -44,37 +43,92 @@ pub enum Action {
     },
 }
 
-mod serde_bytes_32 {
-    pub fn serialize<S>(value: &[u8; 32], serializer: S) -> Result<S::Ok, S::Error>
+mod serde_u256 {
+    use ethnum::U256;
+    use serde::{
+        de::{self, SeqAccess, Visitor},
+        Deserializer, Serializer,
+    };
+
+    pub fn serialize<S>(value: &U256, serializer: S) -> Result<S::Ok, S::Error>
     where
-        S: serde::ser::Serializer,
+        S: Serializer,
     {
-        serializer.serialize_bytes(value)
+        serializer.serialize_bytes(&value.to_le_bytes())
     }
 
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<[u8; 32], D::Error>
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<U256, D::Error>
     where
-        D: serde::Deserializer<'de>,
+        D: Deserializer<'de>,
     {
-        struct BytesVisitor;
+        deserializer.deserialize_bytes(U256Visitor {})
+    }
 
-        impl<'de> serde::de::Visitor<'de> for BytesVisitor {
-            type Value = [u8; 32];
+    struct U256Visitor {}
 
-            fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-                f.write_str("[u8; 32]")
-            }
+    impl<'de> Visitor<'de> for U256Visitor {
+        type Value = U256;
 
-            fn visit_bytes<E>(self, value: &[u8]) -> Result<Self::Value, E>
-            where
-                E: serde::de::Error,
-            {
-                value
-                    .try_into()
-                    .map_err(|_| serde::de::Error::invalid_length(value.len(), &self))
-            }
+        fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            f.write_str("32 bytes in little endian")
         }
 
-        deserializer.deserialize_bytes(BytesVisitor)
+        fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            let bytes = v
+                .try_into()
+                .map_err(|_| E::invalid_length(v.len(), &self))?;
+            Ok(U256::from_le_bytes(bytes))
+        }
+
+        fn visit_seq<S>(self, mut seq: S) -> Result<Self::Value, S::Error>
+        where
+            S: SeqAccess<'de>,
+        {
+            let mut bytes = Vec::with_capacity(32);
+            while let Some(b) = seq.next_element()? {
+                bytes.push(b);
+            }
+            Ok(U256::from_le_bytes(
+                bytes
+                    .try_into()
+                    .map_err(|_| de::Error::custom("Invalid U256 value"))?,
+            ))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn roundtrip_bincode() {
+        let action = Action::EvmSetStorage {
+            address: Default::default(),
+            index: U256::from_le_bytes([
+                255, 46, 185, 41, 144, 201, 3, 36, 227, 18, 148, 147, 106, 131, 110, 6, 229, 235,
+                44, 154, 71, 124, 159, 144, 47, 119, 77, 5, 154, 49, 23, 54,
+            ]),
+            value: Default::default(),
+        };
+        let serialized = bincode::serialize(&action).unwrap();
+        let _deserialized: Action = bincode::deserialize(&serialized).unwrap();
+    }
+
+    #[test]
+    fn roundtrip_json() {
+        let action = Action::EvmSetStorage {
+            address: Default::default(),
+            index: U256::from_le_bytes([
+                255, 46, 185, 41, 144, 201, 3, 36, 227, 18, 148, 147, 106, 131, 110, 6, 229, 235,
+                44, 154, 71, 124, 159, 144, 47, 119, 77, 5, 154, 49, 23, 54,
+            ]),
+            value: Default::default(),
+        };
+        let serialized = serde_json::to_string(&action).unwrap();
+        let _deserialized: Action = serde_json::from_str(&serialized).unwrap();
     }
 }

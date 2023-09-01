neonlabsorg/neon-evm

commit d14c748e4401904b78fb104a52756967f3a6bab6

Debug traces fixes (#75)
* Filling `used_gas` in debug trace

* Added `result` and `exit_status` to tracing output

* Added support of `enable_return_data` argument

* Moved `return_data` to `VMExecutedOperation`

* Moved `return_data` to `FullTraceData`

* Transformed to library

* Added support for block and state overrides during emulation

* Made using commands in library more convenient

* Return structured data for library

* Exposed `NeonCliError`

* Implemented `Debug` and `Clone` for `EmulationResult`

* Renamed `state_overrides`

* Added support of `TraceConfig` and `TraceCallConfig` from STDIN

* Added lifetime to `TracedCall`

* Removed references from `TracedCall` and `EmulationResult`

* Exposed `TransactionParams` and `TransactionHashParams`

* WIP: Block tracing support

* Implemented deserialization of block from RLP

* Implemented `trace-block-by-slot` and `trace-block-by-hash` commands

* Merged `develop`, fixed compilation and clippy errors

* Removed `trace-block-by-hash` command

* Removed `get_block_transactions` method from `Rpc`

* Applied rust-fmt formatting

* Reverted (removed let..else)

* Parse slot in CLI on start

* Reduced nesting

* Applied formatting

* Fixed bug with using BOOL in Postgres expressions

* Changed slot to `i64` for Postgres param

* Fixed Neon CLI emulation calls in tests

* Trace next block instead of trace block by slot

* Trace next block instead of trace block by slot

* Fixed compilation errors, bugs, refactored, added implementation of `trace-next-block`

* Exposed request models in the library

* Removed `TxParamsRequestModel` from `EmulateHashRequestModel`

* Simplified code, enforced type checking

* Implemented constructor for `EmulationParamsRequestModel`

* Renamed `TraceNextBlockParamsRequest` → `TraceNextBlockRequestModel`

* Fixed clippy warnings

* Fixed clippy warnings

* Fixed clippy warnings

evm_loader/lib/src/types/tracer_ch_db.rs
@@ -230,17 +230,16 @@ impl ClickHouseDb {
     async fn get_account_rooted_slot(&self, key: &str, slot: u64) -> ChResult<Option<u64>> {
         info!("get_account_rooted_slot {{ key: {key}, slot: {slot} }}");
         let query = r#"
-            SELECT b.slot
-            FROM events.update_slot AS b
-            WHERE (b.slot IN (
-                      SELECT a.slot
-                      FROM events.update_account_distributed AS a
-                      WHERE (a.pubkey = ?)
-                        AND (a.slot <= ?)
-                      ORDER BY a.pubkey, a.slot DESC
-                      LIMIT 1000))
-              AND (b.status = 'Rooted')
-            ORDER BY b.slot DESC
+            SELECT DISTINCT slot
+            FROM events.update_account_distributed
+            WHERE pubkey = ?
+                AND slot <= ?
+                AND slot IN (
+                    SELECT slot
+                    FROM events.update_slot
+                    WHERE status = 'Rooted'
+                )
+            ORDER BY slot DESC
             LIMIT 1
         "#;
 

evm_loader/api/src/api_server/handlers/emulate.rs
@@ -2,7 +2,9 @@ use axum::{http::StatusCode, Json};
 use std::convert::Into;
 
 use crate::{
-    commands::emulate as EmulateCommand, context, types::request_models::EmulateRequestModel,
+    commands::emulate as EmulateCommand,
+    context,
+    types::{request_models::EmulateRequestModel, trace::TraceCallConfig},
     NeonApiState,
 };
 
@@ -14,27 +16,28 @@ pub async fn emulate(
 ) -> (StatusCode, Json<serde_json::Value>) {
     let tx = emulate_request.tx_params.into();
 
-    let (rpc_client, blocking_rpc_client) =
-        match context::build_rpc_client(&state.config, emulate_request.slot) {
-            Ok(rpc_client) => rpc_client,
-            Err(e) => return process_error(StatusCode::BAD_REQUEST, &e),
-        };
+    let rpc_client = match context::build_rpc_client(&state.config, emulate_request.slot) {
+        Ok(rpc_client) => rpc_client,
+        Err(e) => return process_error(StatusCode::BAD_REQUEST, &e),
+    };
 
-    let context = context::create(rpc_client, state.config.clone(), blocking_rpc_client);
+    let context = context::create(rpc_client, state.config.clone());
 
     let (token, chain, steps, accounts, solana_accounts) =
         parse_emulation_params(&state.config, &context, &emulate_request.emulation_params).await;
 
     process_result(
         &EmulateCommand::execute(
-            &state.config,
-            &context,
+            context.rpc_client.as_ref(),
+            state.config.evm_loader,
             tx,
             token,
             chain,
             steps,
+            state.config.commitment,
             &accounts,
             &solana_accounts,
+            TraceCallConfig::default(),
         )
         .await
         .map_err(Into::into),

evm_loader/api/src/api_server/handlers/emulate_hash.rs
@@ -2,7 +2,9 @@ use axum::{http::StatusCode, Json};
 use std::convert::Into;
 
 use crate::{
-    commands::emulate as EmulateCommand, context, types::request_models::EmulateHashRequestModel,
+    commands::emulate as EmulateCommand,
+    context,
+    types::{request_models::EmulateHashRequestModel, trace::TraceCallConfig},
     NeonApiState,
 };
 
@@ -12,7 +14,7 @@ pub async fn emulate_hash(
     axum::extract::State(state): axum::extract::State<NeonApiState>,
     Json(emulate_hash_request): Json<EmulateHashRequestModel>,
 ) -> (StatusCode, Json<serde_json::Value>) {
-    let (rpc_client, blocking_rpc_client) =
+    let rpc_client =
         match context::build_hash_rpc_client(&state.config, &emulate_hash_request.hash).await {
             Ok(rpc_client) => rpc_client,
             Err(e) => return process_error(StatusCode::BAD_REQUEST, &e),
@@ -28,7 +30,7 @@ pub async fn emulate_hash(
         }
     };
 
-    let context = context::create(rpc_client, state.config.clone(), blocking_rpc_client);
+    let context = context::create(rpc_client, state.config.clone());
 
     let (token, chain, steps, accounts, solana_accounts) = parse_emulation_params(
         &state.config,
@@ -39,14 +41,16 @@ pub async fn emulate_hash(
 
     process_result(
         &EmulateCommand::execute(
-            &state.config,
-            &context,
+            context.rpc_client.as_ref(),
+            state.config.evm_loader,
             tx,
             token,
             chain,
             steps,
+            state.config.commitment,
             &accounts,
             &solana_accounts,
+            TraceCallConfig::default(),
         )
         .await
         .map_err(Into::into),

evm_loader/api/src/api_server/handlers/get_ether_account_data.rs
@@ -13,17 +13,20 @@ pub async fn get_ether_account_data(
     Query(req_params): Query<GetEtherRequest>,
     State(state): State<NeonApiState>,
 ) -> (StatusCode, Json<serde_json::Value>) {
-    let (rpc_client, blocking_rpc_client) =
-        match context::build_rpc_client(&state.config, req_params.slot) {
-            Ok(rpc_client) => rpc_client,
-            Err(e) => return process_error(StatusCode::BAD_REQUEST, &e),
-        };
+    let rpc_client = match context::build_rpc_client(&state.config, req_params.slot) {
+        Ok(rpc_client) => rpc_client,
+        Err(e) => return process_error(StatusCode::BAD_REQUEST, &e),
+    };
 
-    let context = context::create(rpc_client, state.config.clone(), blocking_rpc_client);
+    let context = context::create(rpc_client, state.config.clone());
 
     process_result(
-        &GetEtherAccountDataCommand::execute(&state.config, &context, &req_params.ether)
-            .await
-            .map_err(Into::into),
+        &GetEtherAccountDataCommand::execute(
+            context.rpc_client.as_ref(),
+            &state.config.evm_loader,
+            &req_params.ether,
+        )
+        .await
+        .map_err(Into::into),
     )
 }

evm_loader/api/src/api_server/handlers/get_storage_at.rs
@@ -14,18 +14,17 @@ pub async fn get_storage_at(
     Query(req_params): Query<GetStorageAtRequest>,
     State(state): State<NeonApiState>,
 ) -> (StatusCode, Json<serde_json::Value>) {
-    let (rpc_client, blocking_rpc_client) =
-        match context::build_rpc_client(&state.config, req_params.slot) {
-            Ok(rpc_client) => rpc_client,
-            Err(e) => return process_error(StatusCode::BAD_REQUEST, &e),
-        };
+    let rpc_client = match context::build_rpc_client(&state.config, req_params.slot) {
+        Ok(rpc_client) => rpc_client,
+        Err(e) => return process_error(StatusCode::BAD_REQUEST, &e),
+    };
 
-    let context = context::create(rpc_client, state.config.clone(), blocking_rpc_client);
+    let context = context::create(rpc_client, state.config.clone());
 
     process_result(
         &GetStorageAtCommand::execute(
-            &state.config,
-            &context,
+            context.rpc_client.as_ref(),
+            &state.config.evm_loader,
             req_params.contract_id,
             &req_params.index,
         )

evm_loader/api/src/api_server/handlers/mod.rs
@@ -21,6 +21,7 @@ pub mod get_ether_account_data;
 pub mod get_storage_at;
 pub mod trace;
 pub mod trace_hash;
+pub mod trace_next_block;
 
 #[derive(Debug)]
 pub struct NeonApiError(pub NeonError);
@@ -127,13 +128,7 @@ fn process_result<T: Serialize>(
                 "value": value,
             })),
         ),
-        Err(e) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            Json(json!({
-                "result": "error",
-                "error": e.0.to_string(),
-            })),
-        ),
+        Err(e) => process_error(StatusCode::INTERNAL_SERVER_ERROR, &e.0),
     }
 }
 

evm_loader/api/src/api_server/handlers/trace.rs
@@ -1,6 +1,7 @@
 use axum::{http::StatusCode, Json};
 use std::convert::Into;
 
+use crate::commands::trace::trace_transaction;
 use crate::{context, types::request_models::TraceRequestModel, NeonApiState};
 
 use super::{parse_emulation_params, process_error, process_result};
@@ -11,13 +12,13 @@ pub async fn trace(
 ) -> (StatusCode, Json<serde_json::Value>) {
     let tx = trace_request.emulate_request.tx_params.into();
 
-    let (rpc_client, blocking_rpc_client) =
+    let rpc_client =
         match context::build_rpc_client(&state.config, trace_request.emulate_request.slot) {
             Ok(rpc_client) => rpc_client,
             Err(e) => return process_error(StatusCode::BAD_REQUEST, &e),
         };
 
-    let context = context::create(rpc_client, state.config.clone(), blocking_rpc_client);
+    let context = context::create(rpc_client, state.config.clone());
 
     let (token, chain, steps, accounts, solana_accounts) = parse_emulation_params(
         &state.config,
@@ -27,15 +28,17 @@ pub async fn trace(
     .await;
 
     process_result(
-        &crate::commands::trace::execute(
-            &state.config,
-            &context,
+        &trace_transaction(
+            context.rpc_client.as_ref(),
+            state.config.evm_loader,
             tx,
             token,
             chain,
             steps,
+            state.config.commitment,
             &accounts,
             &solana_accounts,
+            trace_request.trace_call_config.unwrap_or_default(),
         )
         .await
         .map_err(Into::into),

evm_loader/api/src/api_server/handlers/trace_hash.rs
@@ -2,6 +2,7 @@ use std::convert::Into;
 
 use crate::{context, types::request_models::TraceHashRequestModel, NeonApiState};
 use axum::{http::StatusCode, Json};
+use neon_lib::commands::trace::trace_transaction;
 
 use super::{parse_emulation_params, process_error, process_result};
 
@@ -9,13 +10,13 @@ pub async fn trace_hash(
     axum::extract::State(state): axum::extract::State<NeonApiState>,
     Json(trace_hash_request): Json<TraceHashRequestModel>,
 ) -> (StatusCode, Json<serde_json::Value>) {
-    let (rpc_client, blocking_rpc_client) = match context::build_hash_rpc_client(
+    let rpc_client = match context::build_hash_rpc_client(
         &state.config,
         &trace_hash_request.emulate_hash_request.hash,
     )
     .await
     {
-        Ok((rpc_client, blocking_rpc_client)) => (rpc_client, blocking_rpc_client),
+        Ok(rpc_client) => rpc_client,
         Err(e) => return process_error(StatusCode::BAD_REQUEST, &e),
     };
 
@@ -29,7 +30,7 @@ pub async fn trace_hash(
         }
     };
 
-    let context = context::create(rpc_client, state.config.clone(), blocking_rpc_client);
+    let context = context::create(rpc_client, state.config.clone());
 
     let (token, chain, steps, accounts, solana_accounts) = parse_emulation_params(
         &state.config,
@@ -39,15 +40,17 @@ pub async fn trace_hash(
     .await;
 
     process_result(
-        &crate::commands::trace::execute(
-            &state.config,
-            &context,
+        &trace_transaction(
+            context.rpc_client.as_ref(),
+            state.config.evm_loader,
             tx,
             token,
             chain,
             steps,
+            state.config.commitment,
             &accounts,
             &solana_accounts,
+            trace_hash_request.trace_config.unwrap_or_default().into(),
         )
         .await
         .map_err(Into::into),

commit 1769f16d15a932e3597c5b4763cb62c3a3e32a0e

NDEV-2116: Fix performance regressions from #75

evm_loader/lib/src/types/tracer_ch_db.rs
@@ -230,17 +230,15 @@ impl ClickHouseDb {
     async fn get_account_rooted_slot(&self, key: &str, slot: u64) -> ChResult<Option<u64>> {
         info!("get_account_rooted_slot {{ key: {key}, slot: {slot} }}");
         let query = r#"
-            SELECT DISTINCT slot
-            FROM events.update_account_distributed
-            WHERE pubkey = ?
-                AND slot <= ?
-                AND slot IN (
-                    SELECT slot
-                    FROM events.update_slot
-                    WHERE status = 'Rooted'
-                )
-            ORDER BY slot DESC
-            LIMIT 1
+            SELECT max(b.slot)
+            FROM events.update_slot AS b
+            WHERE b.slot IN (SELECT a.slot
+                             FROM events.update_account_distributed AS a
+                             WHERE a.pubkey = ?
+                               AND a.slot <= ?
+                             ORDER BY a.slot DESC
+                             LIMIT 1000)
+              AND b.status = 'Rooted'
         "#;
 
         let time_start = Instant::now();
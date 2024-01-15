88c56aea99d9438525bff68f93a9daaef6679bf3
Andrei Silviu Dragnea <andreidragnea.dev@gmail.com>
2024-01-15 02:17:40 +0200

    Generate constants using opcode_table! macro

--- evm_loader/program/src/evm/mod.rs
+++ evm_loader/program/src/evm/mod.rs
@@ -15,7 +15,7 @@ pub use buffer::Buffer;
 use crate::evm::tracing::TracerTypeOpt;
 use crate::{
     error::{build_revert_message, Error, Result},
-    evm::{opcode::Action, opcode_table::OPCODE_STOP, precompile::is_precompile_address},
+    evm::{opcode::Action, opcode_table::STOP, precompile::is_precompile_address},
     types::{Address, Transaction},
 };
 
@@ -395,11 +395,7 @@ impl<B: Database> Machine<B> {
                     break ExitStatus::StepLimit;
                 }
 
-                let opcode = self
-                    .execution_code
-                    .get(self.pc)
-                    .copied()
-                    .unwrap_or(OPCODE_STOP);
+                let opcode = self.execution_code.get(self.pc).copied().unwrap_or(STOP);
 
                 tracing_event!(
                     self,

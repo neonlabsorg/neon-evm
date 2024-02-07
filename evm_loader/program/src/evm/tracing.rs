2e160b263565ab3680f8c8cd82ff3df9660e7082
Andrei Silviu Dragnea <andreidragnea.dev@gmail.com>
2024-02-07 11:58:02 +0200

    Remove more unused code

--- evm_loader/program/src/evm/tracing.rs
+++ evm_loader/program/src/evm/tracing.rs
@@ -1,4 +1,3 @@
-use std::cell::RefCell;
 use std::collections::BTreeMap;
 use std::fmt::Debug;
 
@@ -78,37 +77,4 @@ pub enum Event {
         index: U256,
         value: [u8; 32],
     },
-}
-
-#[derive(Default, Debug)]
-pub struct StorageStateTracer {
-    initial_storage: RefCell<BTreeMap<Address, BTreeMap<H256, H256>>>,
-    final_storage: RefCell<BTreeMap<Address, BTreeMap<H256, H256>>>,
-}
-
-impl StorageStateTracer {
-    pub fn read_storage(&self, address: Address, index: U256, value: [u8; 32]) {
-        let mut initial_storage = self.initial_storage.borrow_mut();
-        let account_initial_storage = initial_storage.entry(address).or_default();
-
-        account_initial_storage
-            .entry(H256::from(index.to_be_bytes()))
-            .or_insert_with(|| H256::from(value));
-    }
-
-    pub fn write_storage(&self, address: Address, index: U256, value: [u8; 32]) {
-        self.final_storage
-            .borrow_mut()
-            .entry(address)
-            .or_default()
-            .insert(H256::from(index.to_be_bytes()), H256::from(value));
-    }
-
-    pub fn initial_storage_for_address(&self, address: &Address) -> Option<BTreeMap<H256, H256>> {
-        self.initial_storage.borrow().get(address).cloned()
-    }
-
-    pub fn final_storage_for_address(&self, address: &Address) -> Option<BTreeMap<H256, H256>> {
-        self.final_storage.borrow().get(address).cloned()
-    }
 }

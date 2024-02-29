112d305f8719609853b4d5ed75a4eda53c04c345
Anton Lisanin <lisanin.anton@gmail.com>
2024-02-29 17:16:47 +0300

    New accounts migration system

--- evm_loader/program/src/account/mod.rs
+++ evm_loader/program/src/account/mod.rs
@@ -2,18 +2,19 @@ use crate::error::{Error, Result};
 use crate::types::Address;
 use solana_program::account_info::AccountInfo;
 use solana_program::pubkey::Pubkey;
+use solana_program::rent::Rent;
 use std::cell::{Ref, RefMut};
 
 pub use crate::config::ACCOUNT_SEED_VERSION;
 
 pub use ether_balance::{BalanceAccount, Header as BalanceHeader};
 pub use ether_contract::{AllocateResult, ContractAccount, Header as ContractHeader};
-pub use ether_storage::{StorageCell, StorageCellAddress};
-pub use holder::Holder;
+pub use ether_storage::{Header as StorageCellHeader, StorageCell, StorageCellAddress};
+pub use holder::{Header as HolderHeader, Holder};
 pub use incinerator::Incinerator;
 pub use operator::Operator;
 pub use state::{AccountsStatus, StateAccount};
-pub use state_finalized::StateFinalizedAccount;
+pub use state_finalized::{Header as StateFinalizedHeader, StateFinalizedAccount};
 pub use treasury::{MainTreasury, Treasury};
 
 use self::program::System;
@@ -36,12 +37,13 @@ pub const TAG_STATE: u8 = 24;
 pub const TAG_STATE_FINALIZED: u8 = 32;
 pub const TAG_HOLDER: u8 = 52;
 
-pub const TAG_ACCOUNT_BALANCE: u8 = 61;
-pub const TAG_ACCOUNT_CONTRACT: u8 = 71;
-pub const TAG_STORAGE_CELL: u8 = 44;
+pub const TAG_ACCOUNT_BALANCE: u8 = 60;
+pub const TAG_ACCOUNT_CONTRACT: u8 = 70;
+pub const TAG_STORAGE_CELL: u8 = 43;
 
-pub const ACCOUNT_PREFIX_LEN: usize = 1/*tag*/ + 4/*revision*/;
-pub const HOLDER_PREFIX_LEN: usize = 1/*tag*/ + 1/*reserved*/;
+const TAG_OFFSET: usize = 0;
+const HEADER_VERSION_OFFSET: usize = 1;
+pub const ACCOUNT_PREFIX_LEN: usize = 1/*tag*/ + 1/*header version*/;
 
 #[inline]
 fn section<'r, T>(account: &'r AccountInfo<'_>, offset: usize) -> Ref<'r, T> {
@@ -73,6 +75,74 @@ fn section_mut<'r, T>(account: &'r AccountInfo<'_>, offset: usize) -> RefMut<'r,
     })
 }
 
+trait AccountHeader {
+    const VERSION: u8;
+}
+struct NoHeader {}
+impl AccountHeader for NoHeader {
+    const VERSION: u8 = 0;
+}
+
+#[inline]
+fn header<'r, T: AccountHeader>(account: &'r AccountInfo<'_>) -> Ref<'r, T> {
+    section(account, ACCOUNT_PREFIX_LEN)
+}
+
+#[inline]
+fn header_mut<'r, T: AccountHeader>(account: &'r AccountInfo<'_>) -> RefMut<'r, T> {
+    section_mut(account, ACCOUNT_PREFIX_LEN)
+}
+
+fn expand_header<'a, From: AccountHeader, To: AccountHeader>(
+    account: &AccountInfo<'a>,
+    rent: &Rent,
+    db: &AccountsDB<'a>,
+) -> Result<()> {
+    let from_len = std::mem::size_of::<From>();
+    let to_len = std::mem::size_of::<To>();
+
+    assert!(to_len >= from_len);
+    assert!(account.data_len() >= ACCOUNT_PREFIX_LEN + from_len);
+
+    let data_len = account.data_len() - ACCOUNT_PREFIX_LEN - from_len;
+    let required_len = ACCOUNT_PREFIX_LEN + to_len + data_len;
+    assert!(required_len >= account.data_len());
+
+    account.realloc(required_len, false)?;
+
+    let minimum_balance = rent.minimum_balance(required_len);
+    if account.lamports() < minimum_balance {
+        let required_lamports = minimum_balance - account.lamports();
+
+        let system = db.system();
+        let operator = db.operator();
+        system.transfer(operator, account, required_lamports)?;
+    }
+
+    unsafe {
+        use solana_program::program_memory::sol_memmove;
+
+        let ptr = account.try_borrow_mut_data()?.as_mut_ptr();
+        let header_ptr = ptr.add(ACCOUNT_PREFIX_LEN);
+        let src = header_ptr.add(from_len);
+        let dst = header_ptr.add(to_len);
+        sol_memmove(dst, src, data_len);
+
+        src.write_bytes(0, to_len - from_len);
+
+        let version_ptr = ptr.add(HEADER_VERSION_OFFSET);
+        version_ptr.write(To::VERSION);
+    }
+
+    Ok(())
+}
+
+fn header_version(info: &AccountInfo) -> u8 {
+    // This is used only inside the module and account validation should be already done
+    let data = info.data.borrow();
+    data[HEADER_VERSION_OFFSET]
+}
+
 pub fn tag(program_id: &Pubkey, info: &AccountInfo) -> Result<u8> {
     if info.owner != program_id {
         return Err(Error::AccountInvalidOwner(*info.key, *program_id));
@@ -83,16 +153,17 @@ pub fn tag(program_id: &Pubkey, info: &AccountInfo) -> Result<u8> {
         return Err(Error::AccountInvalidData(*info.key));
     }
 
-    Ok(data[0])
+    Ok(data[TAG_OFFSET])
 }
 
-pub fn set_tag(program_id: &Pubkey, info: &AccountInfo, tag: u8) -> Result<()> {
+pub fn set_tag(program_id: &Pubkey, info: &AccountInfo, tag: u8, header_version: u8) -> Result<()> {
     assert_eq!(info.owner, program_id);
 
     let mut data = info.try_borrow_mut_data()?;
     assert!(data.len() >= ACCOUNT_PREFIX_LEN);
 
-    data[0] = tag;
+    data[TAG_OFFSET] = tag;
+    data[HEADER_VERSION_OFFSET] = header_version;
 
     Ok(())
 }
@@ -108,34 +179,17 @@ pub fn validate_tag(program_id: &Pubkey, info: &AccountInfo, tag: u8) -> Result<
 }
 
 pub fn revision(program_id: &Pubkey, info: &AccountInfo) -> Result<u32> {
-    if info.owner != program_id {
-        return Err(Error::AccountInvalidOwner(*info.key, *program_id));
+    match tag(program_id, info)? {
+        TAG_STORAGE_CELL => {
+            let cell = StorageCell::from_account(program_id, info.clone())?;
+            Ok(cell.revision())
+        }
+        TAG_ACCOUNT_CONTRACT => {
+            let contract = ContractAccount::from_account(program_id, info.clone())?;
+            Ok(contract.revision())
+        }
+        _ => Ok(0),
     }
-
-    let data = info.try_borrow_data()?;
-    if data.len() < ACCOUNT_PREFIX_LEN {
-        return Err(Error::AccountInvalidData(*info.key));
-    }
-
-    let buffer = arrayref::array_ref![data, 1, 4];
-    Ok(u32::from_le_bytes(*buffer))
-}
-
-pub fn increment_revision(program_id: &Pubkey, info: &AccountInfo) -> Result<()> {
-    if info.owner != program_id {
-        return Err(Error::AccountInvalidOwner(*info.key, *program_id));
-    }
-
-    let mut data = info.try_borrow_mut_data()?;
-    if data.len() < ACCOUNT_PREFIX_LEN {
-        return Err(Error::AccountInvalidData(*info.key));
-    }
-
-    let buffer = arrayref::array_mut_ref![data, 1, 4];
-    let revision = u32::from_le_bytes(*buffer);
-    *buffer = (revision + 1).to_le_bytes();
-
-    Ok(())
 }
 
 /// # Safety

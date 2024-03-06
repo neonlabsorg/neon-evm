549540f362f2d89193c80ec9c2f9b5ca9ceb6813
Vladimir Bugaev <boogvar@gmail.com>
2024-03-06 20:36:06 +0700

    emulate response has been fixed

--- evm_loader/lib/src/abi/emulate.rs
+++ evm_loader/lib/src/abi/emulate.rs
@@ -5,15 +5,16 @@ use crate::config::APIOptions;
 use crate::rpc::Rpc;
 use crate::tracing::tracers::TracerTypeEnum;
 use crate::{types::EmulateApiRequest, NeonResult};
-use serde_json::Value;
 
 pub async fn execute(
     rpc: &(impl Rpc + BuildConfigSimulator),
     config: &APIOptions,
     params: &str,
-) -> NeonResult<(EmulateResponse, Option<Value>)> {
+) -> NeonResult<EmulateResponse> {
     let params: EmulateApiRequest =
         serde_json::from_str(params).map_err(|_| params_to_neon_error(params))?;
 
-    emulate::execute(rpc, config.evm_loader, params.body, None::<TracerTypeEnum>).await
+    emulate::execute(rpc, config.evm_loader, params.body, None::<TracerTypeEnum>)
+    .await
+    .map(|(response, _)| response)
 }

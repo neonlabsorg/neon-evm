bd07441b6f43948afd9647df463844e66b55754b
gigimon <gigimon4ik@gmail.com>
2024-03-13 13:27:51 +0300

    * deploy multi tokens inside docker

--- ci/deploy-multi-tokens.sh
+++ ci/deploy-multi-tokens.sh
@@ -7,7 +7,8 @@ echo "Deploy USDT token"
 USDT_ADDRESS=$(solana address -k /opt/keys/usdt_token_keypair.json)
 
 spl-token -u http://localhost:8899 create-token --decimals 6 /opt/keys/usdt_token_keypair.json
-spl-token -u http://localhost:8899 mint $USDT_ADDRESS 100000
+spl-token -u http://localhost:8899 create-account $USDT_ADDRESS
+spl-token -u http://localhost:8899 mint $USDT_ADDRESS 100000000000
 
 
 echo "Deploy ETH token"
@@ -15,4 +16,5 @@ echo "Deploy ETH token"
 ETH_ADDRESS=$(solana address -k /opt/keys/eth_token_keypair.json)
 
 spl-token -u http://localhost:8899 create-token --decimals 8 /opt/keys/eth_token_keypair.json
-spl-token -u http://localhost:8899 mint $ETH_ADDRESS 100000
+spl-token -u http://localhost:8899 create-account $ETH_ADDRESS
+spl-token -u http://localhost:8899 mint $ETH_ADDRESS 100000000000

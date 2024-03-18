5bf40f84766a245b6ce6629a07aa32344077dab1
Oleg <gigimon4ik@gmail.com>
2024-03-18 11:55:14 +0300

    Update deploy-multi-tokens.sh

--- ci/deploy-multi-tokens.sh
+++ ci/deploy-multi-tokens.sh
@@ -2,19 +2,21 @@
 
 set -em
 
+SOLANA_URL="${SOLANA_URL:-default}" 
+
 echo "Deploy USDT token"
 
 USDT_ADDRESS=$(solana address -k /opt/keys/usdt_token_keypair.json)
 
-spl-token -u http://localhost:8899 create-token --decimals 6 /opt/keys/usdt_token_keypair.json
-spl-token -u http://localhost:8899 create-account $USDT_ADDRESS
-spl-token -u http://localhost:8899 mint $USDT_ADDRESS 100000000000
+spl-token -u $SOLANA_URL create-token --decimals 6 /opt/keys/usdt_token_keypair.json
+spl-token -u $SOLANA_URL create-account $USDT_ADDRESS
+spl-token -u $SOLANA_URL mint $USDT_ADDRESS 100000000000
 
 
 echo "Deploy ETH token"
 
 ETH_ADDRESS=$(solana address -k /opt/keys/eth_token_keypair.json)
 
-spl-token -u http://localhost:8899 create-token --decimals 8 /opt/keys/eth_token_keypair.json
-spl-token -u http://localhost:8899 create-account $ETH_ADDRESS
-spl-token -u http://localhost:8899 mint $ETH_ADDRESS 100000000000
+spl-token -u $SOLANA_URL create-token --decimals 8 /opt/keys/eth_token_keypair.json
+spl-token -u $SOLANA_URL create-account $ETH_ADDRESS
+spl-token -u $SOLANA_URL mint $ETH_ADDRESS 100000000000

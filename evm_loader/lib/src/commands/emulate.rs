56c932839e671e0105ce0e3c60f8e98f3e300280
Andrei Silviu Dragnea <andreidragnea.dev@gmail.com>
2024-05-20 11:55:30 +0300

    cargo fmt

--- evm_loader/lib/src/commands/emulate.rs
+++ evm_loader/lib/src/commands/emulate.rs
@@ -101,13 +101,9 @@ pub async fn execute<T: Tracer>(
     let result = emulate_trx(emulate_request.tx.clone(), &mut storage, step_limit, tracer).await?;
 
     if storage.is_timestamp_used() {
-        let mut storage2 = EmulatorAccountStorage::new_from_other(
-            &storage,
-            5,
-            3,
-            emulate_request.tx.chain_id,
-        )
-        .await?;
+        let mut storage2 =
+            EmulatorAccountStorage::new_from_other(&storage, 5, 3, emulate_request.tx.chain_id)
+                .await?;
         if let Ok(result2) = emulate_trx(
             emulate_request.tx,
             &mut storage2,

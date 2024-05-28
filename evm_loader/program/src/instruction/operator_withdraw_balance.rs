858f9772b977ca9959026f1e3d39b68734d41ed1
Evgeniy Zdanovich <zdanovichevgeny@gmail.com>
2024-05-28 11:14:51 +0200

    Fix after rebase on develop.

--- evm_loader/program/src/instruction/operator_withdraw_balance.rs
+++ evm_loader/program/src/instruction/operator_withdraw_balance.rs
@@ -21,6 +21,13 @@ pub fn process<'a>(
     operator_balance.validate_owner(&operator)?;
     operator_balance.withdraw(&mut target_balance)?;
 
-    let accounts_db = AccountsDB::new(&[], operator, Some(operator_balance), Some(system), None);
+    let accounts_db = AccountsDB::new(
+        &[],
+        operator,
+        Some(operator_balance),
+        Some(system),
+        None,
+        None,
+    );
     target_balance.increment_revision(&Rent::get()?, &accounts_db)
 }

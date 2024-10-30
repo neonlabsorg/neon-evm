044b4bfe33a670874b0136ead9e7d5c30792facc
Vladimir Bugaev <boogvar@gmail.com>
2024-10-30 20:15:36 +0700

    add methods to the erc client

--- evm_loader/rpc-client/src/lib.rs
+++ evm_loader/rpc-client/src/lib.rs
@@ -10,14 +10,15 @@ pub use error::NeonRpcClientError;
 
 use async_trait::async_trait;
 use neon_lib::{
+    build_info_common::SlimBuildInfo,
     commands::{
         emulate::EmulateResponse, get_balance::GetBalanceResponse, get_config::GetConfigResponse,
         get_contract::GetContractResponse, get_holder::GetHolderResponse,
-        get_storage_at::GetStorageAtReturn,
+        get_storage_at::GetStorageAtReturn, simulate_solana::SimulateSolanaResponse,
     },
     types::{
         EmulateApiRequest, GetBalanceRequest, GetContractRequest, GetHolderRequest,
-        GetStorageAtRequest,
+        GetStorageAtRequest, SimulateSolanaRequest,
     },
 };
 
@@ -41,4 +42,10 @@ pub trait NeonRpcClient {
         params: GetStorageAtRequest,
     ) -> NeonRpcClientResult<GetStorageAtReturn>;
     async fn trace(&self, params: EmulateApiRequest) -> NeonRpcClientResult<serde_json::Value>;
+    async fn simulate_solana(
+        &self,
+        params: SimulateSolanaRequest,
+    ) -> NeonRpcClientResult<SimulateSolanaResponse>;
+    async fn build_info(&self) -> NeonRpcClientResult<SlimBuildInfo>;
+    async fn lib_build_info(&self) -> NeonRpcClientResult<SlimBuildInfo>;
 }

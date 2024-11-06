9fc8c0880516864da6d89e4a2fa10e632ecb6402
AnastasiyaTarasova <tanastasiiaa@gmail.com>
2024-11-06 21:46:18 +0400

    Revert "Development log_data macros"
    This reverts commit a40d749e4ed7717649d6bce90d0856a9fb3325fb.

--- evm_loader/program-macro/src/lib.rs
+++ evm_loader/program-macro/src/lib.rs
@@ -2,7 +2,6 @@
 #![deny(clippy::all, clippy::pedantic, clippy::nursery)]
 
 mod config_parser;
-mod derive_log_data;
 
 use std::collections::BTreeMap;
 
@@ -258,21 +257,4 @@ fn is_composite_vector_type(ty: &Type) -> bool {
         return false;
     }
     false
-}
-#[proc_macro_derive(LogData)]
-pub fn derive_log_data(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-
-    let ident = input.ident.clone();
-
-    let implementation = crate::derive_log_data::gen_impl(input);
-
-    quote! {
-        impl LogData for #ident {
-            fn log_data(&self) {
-                #implementation
-            }
-        }
-    }
-    .into()
 }

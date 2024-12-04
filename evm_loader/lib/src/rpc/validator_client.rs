63d3c490776f1f5ea807d052bd5d8c666352f836
Novikov Roman <romaetoroma@gmail.com>
2024-12-04 19:02:04 +0400

    Revert "try no CloneRpcClient depl slot 0"
    This reverts commit 11e059ddce6a5dec9e2fb924cd3baafd2a83e61e.

--- evm_loader/lib/src/rpc/validator_client.rs
+++ evm_loader/lib/src/rpc/validator_client.rs
@@ -114,9 +114,9 @@ impl Deref for CloneRpcClient {
 
 #[async_trait(?Send)]
 impl Rpc for CloneRpcClient {
-    // async fn get_last_deployed_slot(&self, _program_id: &Pubkey) -> ClientResult<Option<u64>> {
-    //     Ok(Some(0))
-    // }
+    async fn get_last_deployed_slot(&self, _program_id: &Pubkey) -> ClientResult<Option<u64>> {
+        Ok(Some(0))
+    }
     async fn get_account_slice(
         &self,
         key: &Pubkey,

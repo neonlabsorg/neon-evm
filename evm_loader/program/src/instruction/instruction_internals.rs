0f8a457cbae5646ba7afbdbef7ad66effff0ee15
Anton Lisanin <lisanin.anton@gmail.com>
2024-12-06 15:26:34 +0300

    Fix evm state realloc with block timestamp

--- evm_loader/program/src/instruction/instruction_internals.rs
+++ evm_loader/program/src/instruction/instruction_internals.rs
@@ -22,15 +22,27 @@ pub fn allocate_evm(
 ) -> Result<()> {
     storage.reset_steps_executed();
 
-    // Dealloc objects that were potentially alloced in previous iterations before the reset.
+    // Dealloc evm that was potentially alloced in previous iterations before the reset.
     if storage.is_evm_alloced() {
         storage.dealloc_evm::<EvmBackend, NoopEventListener>();
     }
+
+    // Dealloc executor state that was potentially alloced in previous iterations before the reset.
+    // Also, copy the block params for use into the new ExecutorStateData.
+    let mut block_params = None;
     if storage.is_executor_state_alloced() {
+        block_params = Some(storage.read_executor_state().get_block_params());
         storage.dealloc_executor_state();
     }
 
-    let mut state_data = boxx(ExecutorStateData::new(account_storage));
+    let mut state_data = {
+        // Preserve the previous block params.
+        if let Some(block_params) = block_params {
+            boxx(ExecutorStateData::new_with_block_params(block_params))
+        } else {
+            boxx(ExecutorStateData::new(account_storage))
+        }
+    };
     let mut evm_backend = ExecutorState::new(account_storage, &mut state_data);
     let evm = boxx(Evm::new(
         storage.trx(),

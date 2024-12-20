092e98c09cc2f367db3f87b6af56550d1a9c8be6
Dzmitry Zdanovich <dzmitry.zdanovich@neonfoundation.io>
2024-12-20 18:14:23 +0100

    NDEV-3112. Fix bug, add test for deactivated_features

--- evm_loader/lib/src/types/deactivated_features.rs
+++ evm_loader/lib/src/types/deactivated_features.rs
@@ -5,27 +5,60 @@ use solana_client::client_error::Result as ClientResult;
 use solana_sdk::pubkey::Pubkey;
 use std::collections::HashMap;
 use std::sync::Arc;
+use std::time::{Duration, Instant};
 use tokio::sync::Mutex;
 
-static DEACTIVATED_FEATURES_UPDATE_SLOTS: u64 = 24 * 60 * 60; // approximately each 12h
+static DEACTIVATED_FEATURES_PERIOD: Duration = Duration::from_secs(60 * 60); // 1 hour
 
-#[derive(Debug, Eq, PartialEq, Clone, Default)]
+pub trait BoxRpc: Rpc + Sync + Send {}
+impl<T: Rpc + Sync + Send> BoxRpc for T {}
+
 struct DeactivatedFeaturesCache {
     // feature to slot when activated, if not then None
     pub data: HashMap<Pubkey, Option<u64>>,
-    pub last_slot: u64,
+    pub last_update: Instant,
+    pub rpc: Option<Box<dyn BoxRpc>>,
+}
+
+impl Default for DeactivatedFeaturesCache {
+    fn default() -> Self {
+        Self {
+            data: HashMap::new(),
+            last_update: Instant::now()
+                .checked_sub(DEACTIVATED_FEATURES_PERIOD)
+                .unwrap(),
+            rpc: None,
+        }
+    }
 }
 
 impl DeactivatedFeaturesCache {
-    fn should_update(&self, slot: u64) -> bool {
-        self.last_slot >= DEACTIVATED_FEATURES_UPDATE_SLOTS + slot
+    pub fn should_update(&mut self) -> bool {
+        if self.rpc.is_some() {
+            self.last_update + DEACTIVATED_FEATURES_PERIOD <= Instant::now()
+        } else {
+            false
+        }
+    }
+
+    pub fn set_deactivated_features_rpc(&mut self, rpc: impl BoxRpc + 'static) {
+        self.rpc = Some(Box::new(rpc));
+    }
+
+    pub async fn update(&mut self) -> ClientResult<()> {
+        if let Some(ref rpc) = self.rpc {
+            self.last_update = Instant::now();
+            self.data = get_multiple_features(rpc.as_ref()).await?;
+        }
+
+        Ok(())
     }
 }
 
 static DEACTIVATED_FEATURES: Lazy<Arc<Mutex<DeactivatedFeaturesCache>>> =
     Lazy::new(|| Arc::new(Mutex::new(DeactivatedFeaturesCache::default())));
 
-async fn get_multiple_features(rpc: &dyn Rpc) -> ClientResult<HashMap<Pubkey, Option<u64>>> {
+async fn get_multiple_features(rpc: &dyn BoxRpc) -> ClientResult<HashMap<Pubkey, Option<u64>>> {
     let feature_keys: Vec<Pubkey> = solana_sdk::feature_set::FEATURE_NAMES
         .keys()
         .copied()
@@ -45,28 +78,20 @@ async fn get_multiple_features(rpc: &dyn Rpc) -> ClientResult<HashMap<Pubkey, Op
     Ok(result)
 }
 
-async fn deactivated_features_get_instance(
-    rpc: &dyn Rpc,
-) -> ClientResult<DeactivatedFeaturesCache> {
+async fn get_deactivated_features() -> ClientResult<HashMap<Pubkey, Option<u64>>> {
     let mut cache = DEACTIVATED_FEATURES.lock().await;
 
-    let rpc_slot = rpc.get_slot().await?;
-    if cache.should_update(rpc_slot) {
-        cache.last_slot = rpc_slot;
-        cache.data = get_multiple_features(rpc).await?;
-    }
+    if cache.should_update() {
+        cache.update().await?;
+    };
 
-    Ok((*cache).clone())
+    Ok(cache.data.clone())
 }
 
-pub async fn get_deactivated_features(
-    rpc: &dyn Rpc,
-    slot: Option<u64>,
-) -> ClientResult<Vec<Pubkey>> {
-    let features = deactivated_features_get_instance(rpc).await?;
+pub async fn get_deactivated_features_at_slot(slot: Option<u64>) -> ClientResult<Vec<Pubkey>> {
+    let features = get_deactivated_features().await?;
 
     Ok(features
-        .data
         .into_iter()
         .filter_map(|(pubkey, activated_at)| match (slot, activated_at) {
             (_, None) => Some(pubkey),
@@ -80,3 +105,12 @@ pub async fn get_deactivated_features(
         })
         .collect())
 }
+
+pub async fn set_deactivated_features_rpc(rpc: impl BoxRpc + 'static) {
+    let mut cache = DEACTIVATED_FEATURES.lock().await;
+    cache.set_deactivated_features_rpc(rpc);
+}
+
+#[cfg(test)]
+#[path = "../deactivated_features_tests.rs"]
+mod deactivated_features_tests;

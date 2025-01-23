a313244ab8b49e4bbb4274f59c4c0b13c7bf5f6b
Novikov Roman <romaetoroma@gmail.com>
2025-01-23 11:24:52 +0400

    fix description

--- evm_loader/lib/src/tracing/tracers/call_tracer.rs
+++ evm_loader/lib/src/tracing/tracers/call_tracer.rs
@@ -297,7 +297,7 @@ impl Tracer for CallTracer {
         let call_stack_len = self.call_stack.len();
         assert!(
             call_stack_len == 1,
-            "incorrect number of top-level calls {call_stack_len}. and gas {emulator_gas_used}"
+            "incorrect number of top-level calls, call_stack_len== {call_stack_len}, and it must be 1 . (emulator_gas_used:   {emulator_gas_used})"
         );
 
         let call_frame = &mut self.call_stack[0];

d1350e64ccdab4ab4bb564aa8227735a6cc0d438
Novikov Roman <romaetoroma@gmail.com>
2025-01-24 09:35:45 +0400

    for example for test fail

--- evm_loader/lib/src/rpc/mod.rs
+++ evm_loader/lib/src/rpc/mod.rs
@@ -11,18 +11,15 @@ use crate::commands::get_config::{BuildConfigSimulator, ConfigSimulator};
 use crate::{NeonError, NeonResult};
 use async_trait::async_trait;
 
-use crate::types::programs_cache::get_program_programdata_address;
-use crate::types::programs_cache::get_programdata_slot_from_account;
-
 use enum_dispatch::enum_dispatch;
-use evm_loader::solana_program::bpf_loader_upgradeable::UpgradeableLoaderState;
+
 pub use solana_account_decoder::UiDataSliceConfig as SliceConfig;
 use solana_cli::cli::CliError;
-use solana_client::client_error::{ClientErrorKind, Result as ClientResult};
+use solana_client::client_error::Result as ClientResult;
 use solana_sdk::{
     account::Account, message::Message, native_token::lamports_to_sol, pubkey::Pubkey,
 };
-use std::cmp::max;
+
 #[async_trait(?Send)]
 #[enum_dispatch]
 pub trait Rpc {
@@ -32,36 +29,8 @@ pub trait Rpc {
         slice: Option<SliceConfig>,
     ) -> ClientResult<Option<Account>>;
 
-    async fn get_last_deployed_slot(&self, program_id: &Pubkey) -> ClientResult<Option<u64>> {
-        let slice_len = max(
-            std::mem::size_of::<UpgradeableLoaderState>(),
-            UpgradeableLoaderState::size_of_programdata_metadata(),
-        );
-
-        let slice = SliceConfig {
-            offset: 0,
-            length: slice_len,
-        };
-
-        let result = self.get_account_slice(program_id, Some(slice)).await;
-        // bpfv2 and request account from link
-
-        if let Ok(Some(acc)) = result {
-            let slot = if acc.executable {
-                get_programdata_slot_from_account(&acc)
-                    .expect("error")
-                    .expect("No slot info")
-            } else {
-                let pd_addr = get_program_programdata_address(&acc)?.expect("no program info");
-                let rz = self
-                    .get_account_slice(&pd_addr, Some(slice))
-                    .await?
-                    .expect("No account ");
-                get_programdata_slot_from_account(&rz)?.expect("No slice ")
-            };
-            return Ok(Some(slot));
-        }
-        Err(ClientErrorKind::Custom("Not account on slot ".to_string()).into())
+    async fn get_last_deployed_slot(&self, _program_id: &Pubkey) -> ClientResult<Option<u64>> {
+        return Ok(Some(1));
     }
 
     async fn get_account(&self, key: &Pubkey) -> ClientResult<Option<Account>> {

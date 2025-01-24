20f337fda617ee48de5ca60d09f1a7ff6c4621d8
AnastasiyaTarasova <tanastasiiaa@gmail.com>
2025-01-24 16:53:13 +0400

    Revert "entrypoint changes map_err -> inspect_err"
    This reverts commit fb4201247b6b4ae853942fd256b30d9021fb8ac7.

--- evm_loader/program/src/entrypoint.rs
+++ evm_loader/program/src/entrypoint.rs
@@ -53,7 +53,7 @@ fn process_instruction<'a>(
             Err(ProgramError::InvalidInstructionData.into())
         }
     }
-    .inspect_err(|e| {
+    .map_err(|e| {
         e.log_data();
         e
     })
@@ -224,7 +224,7 @@ fn process_instruction<'a>(
             instruction::operator_withdraw_balance::process(program_id, accounts, instruction)
         }
     }
-    .inspect_err(|e| {
+    .map_err(|e| {
         e.log_data();
         e
     })

1db512cb154abb4ee86ff8a42600be4ffa1d6029
Anton Lisanin <lisanin.anton@gmail.com>
2025-02-09 17:18:53 +0300

    Set version to v1.17.4

--- evm_loader/program/src/executor/mod.rs
+++ evm_loader/program/src/executor/mod.rs
@@ -7,6 +7,7 @@ mod synced_state;
 pub mod precompile_extension;
 
 pub use action::Action;
+pub use block_params::BlockParams;
 pub use cache::OwnedAccountInfo;
 pub use state::ExecutorState;
 pub use state::ExecutorStateData;

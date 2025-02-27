34d99959417c564e857dc2b586c15ae952d0d524
AnastasiyaTarasova <tanastasiiaa@gmail.com>
2025-02-27 20:42:28 +0400

    emulate_multiple unwrap -> unwrap_or_default

--- evm_loader/lib/src/commands/emulate_multiple.rs
+++ evm_loader/lib/src/commands/emulate_multiple.rs
@@ -47,7 +47,7 @@ pub async fn execute(
         )
         .await?;
 
-        let accounts = response.accounts_data.take().unwrap();
+        let accounts = response.accounts_data.take().unwrap_or_default();
         for account in accounts {
             overrides.insert(account.pubkey, Some(account.into()));
         }

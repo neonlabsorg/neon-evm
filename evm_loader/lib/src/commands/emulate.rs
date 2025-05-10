25a81cc5229f29f4a8e7a9f1b33aeefebf4834ab
Andrey Falaleev <andrew.falaleev@gmail.com>
2025-05-10 23:41:25 +0100

    api: return STORAGE_GAS and REALLOC_ITERATIONS in emulator result

--- evm_loader/lib/src/commands/emulate.rs
+++ evm_loader/lib/src/commands/emulate.rs
@@ -57,7 +57,9 @@ pub struct EmulateResponse {
     pub result: Vec<u8>,
     pub steps_executed: u64,
     pub used_gas: u64,
+    pub storage_gas: u64,
     pub iterations: u64,
+    pub realloc_iterations: u64,
     pub solana_accounts: Vec<SolanaAccount>,
     pub logs: Vec<Log>,
     pub accounts_data: Option<Vec<AccountData>>,
@@ -86,7 +88,9 @@ impl EmulateResponse {
             result: exit_status.into_result().unwrap_or_default(),
             steps_executed: 0,
             used_gas: 0,
+            storage_gas: 0,
             iterations: 0,
+            realloc_iterations: 0,
             solana_accounts: vec![],
             logs: backend.backend().logs(),
             accounts_data: None,
@@ -255,7 +259,8 @@ async fn calculate_response<T: Rpc + BuildConfigSimulator, Tr: Tracer>(
     let steps_iterations = 1.max(steps_executed.div_ceil(EVM_STEPS_MIN));
 
     let begin_end_iterations = 2;
-    let iterations: u64 = steps_iterations + begin_end_iterations + storage.realloc_iterations;
+    let realloc_iterations = storage.realloc_iterations;
+    let iterations: u64 = steps_iterations + begin_end_iterations + realloc_iterations;
     let iterations_gas = iterations * LAMPORTS_PER_SIGNATURE;
     let treasury_gas = iterations * PAYMENT_TO_TREASURE;
     let storage_gas = storage.get_changes_in_rent()?;
@@ -280,9 +285,11 @@ async fn calculate_response<T: Rpc + BuildConfigSimulator, Tr: Tracer>(
             is_timestamp_number_used: storage.is_timestamp_number_used(),
             steps_executed,
             used_gas,
+            storage_gas,
             solana_accounts,
             result: exit_status.into_result().unwrap_or_default(),
             iterations,
+            realloc_iterations,
             logs,
             accounts_data: None,
         },

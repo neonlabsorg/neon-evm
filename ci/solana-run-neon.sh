0a683f1d9a516864c4822c21d28654331d52321c
Natalia Romanova <nataliaromanova@Natalias-MacBook-Air.local>
2025-06-17 21:13:59 +0100

    change dir name
    change dir name

--- ci/solana-run-neon.sh
+++ ci/solana-run-neon.sh
@@ -42,7 +42,7 @@ VALIDATOR_ARGS=(
   --limit-ledger-size 400000000
 )
 
-LIST_OF_TEST_PROGRAMS=("test_invoke_program" "counter" "cross_program_invocation" "transfer_sol" "transfer_tokens" "alt-updater")
+LIST_OF_TEST_PROGRAMS=("test_invoke_program" "counter" "cross_program_invocation" "transfer_sol" "transfer_tokens" "alt_updater")
 
 for program in "${LIST_OF_TEST_PROGRAMS[@]}"; do
   keypair="${NEON_BIN}/deploy/${program}/${program}-keypair.json"

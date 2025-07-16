14d544ce7b6d5010bbb9ee32c436aa56876bfebd
Mikhail Surin <surinmike@gmail.com>
2025-07-16 16:18:09 +0300

    fix input type for rpc emulate method

--- evm_loader/rpc/src/handlers/emulate_from_holder.rs
+++ evm_loader/rpc/src/handlers/emulate_from_holder.rs
@@ -3,11 +3,11 @@
 use super::invoke;
 use crate::{context::Context, error::NeonRPCError};
 use jsonrpc_v2::{Data, Params};
-use neon_lib::{types::EmulateApiRequest, LibMethod};
+use neon_lib::{types::EmulateFromHolderApiRequest, LibMethod};
 
 pub async fn handle(
     ctx: Data<Context>,
-    Params(params): Params<Vec<EmulateApiRequest>>,
+    Params(params): Params<Vec<EmulateFromHolderApiRequest>>,
 ) -> Result<serde_json::Value, jsonrpc_v2::Error> {
     let param = params.first().ok_or(NeonRPCError::IncorrectParameters())?;
     invoke(

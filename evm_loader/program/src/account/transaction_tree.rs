74235d7caa7537b05d56b1ba7cd4964ac33efd73
Anton Lisanin <lisanin.anton@gmail.com>
2025-07-17 12:00:29 +0300

    Fix scheduled transactions gas

--- evm_loader/program/src/account/transaction_tree.rs
+++ evm_loader/program/src/account/transaction_tree.rs
@@ -3,12 +3,11 @@ use std::mem::size_of;
 
 use super::treasury::Treasury;
 use super::{
-    pda, Account, AccountDispatch, AccountHeader, BalanceAccount, Operator, ACCOUNT_PREFIX_LEN,
-    TAG_TRANSACTION_TREE,
+    pda, program, Account, AccountDispatch, AccountHeader, BalanceAccount, Operator,
+    ACCOUNT_PREFIX_LEN, TAG_TRANSACTION_TREE,
 };
-use crate::account::program;
 use crate::config::{
-    BASE_ITERATIVE_TRANSACTION_COST, TREE_ACCOUNT_DESTROY_FEE, TREE_ACCOUNT_FINISH_TRANSACTION_GAS,
+    BASE_ITERATIVE_TRANSACTION_COST, TREE_ACCOUNT_DESTROY_FEE, TREE_ACCOUNT_FINISH_TRANSACTION_FEE,
     TREE_ACCOUNT_TIMEOUT,
 };
 use crate::error::{Error, Result};
@@ -136,9 +135,7 @@ impl<'a> TransactionTree<'a> {
         clock: &Clock,
     ) -> Result<Self> {
         const MIN_FEE_PER_GAS: U256 = U256::new(1_100_000_000);
-        const MIN_GAS_LIMIT: U256 = U256::new(
-            BASE_ITERATIVE_TRANSACTION_COST as u128 + TREE_ACCOUNT_FINISH_TRANSACTION_GAS as u128,
-        );
+        const MIN_GAS_LIMIT: U256 = U256::new(BASE_ITERATIVE_TRANSACTION_COST as u128);
         const TREE_ACCOUNT_MAX_NODES: usize = 16;
 
         // Validate account
@@ -206,7 +203,10 @@ impl<'a> TransactionTree<'a> {
             space,
             rent,
         )?;
-        system.transfer(destroy_fee_payer, &account, TREE_ACCOUNT_DESTROY_FEE)?;
+
+        let nodes_len = nodes.len() as u64;
+        let fee = TREE_ACCOUNT_DESTROY_FEE + (nodes_len * TREE_ACCOUNT_FINISH_TRANSACTION_FEE);
+        system.transfer(destroy_fee_payer, &account, fee)?;
 
         // Init data
         account.init_tag(TAG_TRANSACTION_TREE, Header::VERSION)?;
@@ -426,6 +426,7 @@ impl<'a> TransactionTree<'a> {
         &mut self,
         hash: [u8; 32],
         result: (Status, solana_program::keccak::Hash),
+        operator: &Operator<'a>,
     ) -> Result<()> {
         use solana_program::keccak::Hash;
 
@@ -448,6 +449,27 @@ impl<'a> TransactionTree<'a> {
         self.update_last_slot(&clock);
 
         self.decrease_parent_count(child_index, status);
+        self.pay_for_end_transaction(operator)
+    }
+
+    fn pay_for_end_transaction(&self, operator: &Operator<'a>) -> Result<()> {
+        let rent = Rent::get()?;
+        let minimum_balance = rent.minimum_balance(self.account.data_len());
+
+        let available_lamports = self
+            .account
+            .lamports()
+            .saturating_sub(minimum_balance)
+            .saturating_sub(TREE_ACCOUNT_DESTROY_FEE);
+
+        if available_lamports < TREE_ACCOUNT_FINISH_TRANSACTION_FEE {
+            return Ok(()); // Not enough funds. This could happen if working with old tree account.
+        }
+
+        let account_info: AccountInfo<'a> = self.account.clone().try_into()?;
+
+        **account_info.lamports.borrow_mut() -= TREE_ACCOUNT_FINISH_TRANSACTION_FEE;
+        **operator.lamports.borrow_mut() += TREE_ACCOUNT_FINISH_TRANSACTION_FEE;
 
         Ok(())
     }
